8e0cd79f405fbec530c72268932d1e18ebae2d5d
jeeyani <74583344+jeeyani@users.noreply.github.com>
2021-10-19 22:16:21 +0900

    2021-10-19 A+B - 7

---
+++ haebum/jeeyani/bruteForce/BJ11021_APlusBMis7.java
@@ -0,0 +1,28 @@
+package jeeyani.bruteForce;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class BJ11021_APlusBMis7 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int tc = Integer.valueOf(br.readLine());
+		
+		StringTokenizer st ;
+		
+		for (int i = 1; i <= tc; i++) {
+		
+			st = new StringTokenizer(br.readLine(), " ");
+			int a = Integer.valueOf(st.nextToken());
+			int b = Integer.valueOf(st.nextToken());
+			
+
+			System.out.println("Case #"+i+": "+(a+b));
+			
+		}
+	}
+
+}
