166164950a7c7d8107cc880e64e116bc5499b3f6
정지원 <skdmldyd23@naver.com>
2021-10-23 12:52:30 +0900

    2021-10-21 A+B-7/블랙잭

---
+++ skdmldyd23/BJ_11021_AplusB.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+
+import java.io.IOException;
+
+import java.io.InputStreamReader;
+
+import java.util.StringTokenizer;
+
+ 
+
+public class BJ_11021_AplusB {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int t = Integer.parseInt(br.readLine());
+		for (int i = 1; i <= t; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			sb.append("Case #").append(i).append(": ")
+			.append(calculation(Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken())))
+			.append("\n");
+		}
+		System.out.println(sb);
+	}
+
+	private static int calculation(int... nums) {
+		int value = 0;
+		for(int num : nums) value += num; 
+		return value;
+
+	}
+
+}
