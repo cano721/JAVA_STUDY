5c238cc60ef9cb8e3c787d8a7b7efe02876af6a7
정지원 <skdmldyd23@naver.com>
2021-10-27 22:30:21 +0900

    2021-10-27 영화감독숌

---
+++ skdmldyd23/D20211027_BJ_1436_영화감독_숌.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class D20211027_BJ_1436_영화감독_숌 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		System.out.println(calculation(n));
+	}
+	
+	public static int calculation(int n) {
+		Integer num = 665;
+		int cnt = 0;
+		while(n > cnt) if((++num).toString().contains("666")) cnt++;
+		return num;
+	}
+}
