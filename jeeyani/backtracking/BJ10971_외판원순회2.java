e6248bf23338ce38fafddf1b810d2286a2a47d71
jeeyani <74583344+jeeyani@users.noreply.github.com>
2021-11-01 22:12:10 +0900

    외판원순회2_코드수정

--- jeeyani/backtracking/BJ10971_외판원순회2.java
+++ jeeyani/backtracking/BJ10971_외판원순회2.java
@@ -28,45 +28,40 @@ public class BJ10971_외판원순회2 {
 				W[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-
-		getCourses(0, 0);
+		
+		// 시작점이 어디든 최소값은 모두 동일함!
+		getCourses(1,1,0, 0);
 		System.out.println(min);
 	}
 
-	private static void getCourses(int idx, int cnt) {
+	private static void getCourses(int start, int now, int cnt, int pay) {
 
-		//모든 경로를 다 돌았다면 종료
-		if (cnt == n) {
-			getMin();
+		//모든 경로를 다 돌았다면 종료 ==> 시작점으로 다시 돌아오는 경우도 확인해줘야함
+		//if (cnt == n) {
+		
+		//다시 시작점으로 돌아온 경우와, 모든 경로를 다 돌았을 경우
+		if(start == now && cnt == n ) {
+			min = Math.min(min, pay);
 			return;
 		}
 
-		for (int i = idx; i < n; i++) {
-
-			if (!visit[i]) {
+		for (int i = 0; i < n; i++) {
+			
+			if(W[now][i] == 0) continue;
+			
+			if(!visit[i] && W[now][i] > 0) {
+				
 				visit[i] = true;
-				getCourses(i + 1, cnt + 1);
-
-				visit[i] = false; // 재귀가 끝나면 비방문으로 변경
+				pay += W[now][i];
+				getCourses(start, i, cnt+1, pay);
+				
+				pay -=W[now][i];
+				visit[i] = false;
+				
 			}
+	
 
 		}
-
-	}
-
-	private static void getMin() {
-		int course = 0;
-
-		for (int i = 0; i < n - 1; i++) {
-			for (int j = i + 1; j < n; j++) {
-
-				if (visit[i] == true && visit[j] == true) {
-					course += W[i][j];
-				}
-			}
-		}
-
-		min = Math.min(min, course);
 
 	}
 
