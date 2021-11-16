ac9cfb3a8114e3b4962f5fa16a70ebeb431b1a80
1223gogo <73056953+1223gogo@users.noreply.github.com>
2021-11-16 23:07:26 +0900

    2021-11-16 부분합

---
+++ gogo1223/queue/n1806_부분합.java
@@ -0,0 +1,42 @@
+package queue;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class n1806_부분합 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		StringTokenizer st = new StringTokenizer(br.readLine()); 
+		
+		int n = Integer.parseInt(st.nextToken());
+		int s = Integer.parseInt(st.nextToken());
+		int[] arr = new int[n];
+		
+		st = new StringTokenizer(br.readLine()); 
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		int start = 0, end = 0, sum = 0;
+		int minLen = Integer.MAX_VALUE;
+		
+		while(true) {
+			if(sum >= s) {
+				minLen = Math.min(end - start, minLen);
+				sum -= arr[start];
+				start++;
+			}
+			else if(end == n) break;
+			else if(sum < s) {
+				sum += arr[end];
+				end++;
+			}
+		}
+		
+		if(minLen == Integer.MAX_VALUE) System.out.println("0");
+		else System.out.println(minLen);
+	}
+
+}
