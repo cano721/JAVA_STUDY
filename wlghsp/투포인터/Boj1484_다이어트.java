57fb24aaf3a7a5fd9b716c8d4b37f4c8e673f7e4
wlghsp <wlghsp@gmail.com>
2021-11-22 02:01:56 +0900

    2021-11-22 다이어트(미완)

---
+++ wlghsp/투포인터/Boj1484_다이어트.java
@@ -0,0 +1,21 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 투포인터.
+
+public class Boj1484_다이어트 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int G = Integer.parseInt(br.readLine());
+        int start = 1;
+        int end = 1;
+        boolean flag = false;
+        while (true) {
+
+        }
+
+    }
+}
