f7efa6d144d64f5ceeec4e83f45c435cc2dd8fb5
wlghsp <wlghsp@gmail.com>
2021-11-24 11:08:08 +0900

    2021-11-24 신나는함수실행(완)

--- wlghsp/DP/Boj9184_신나는함수실행.java
+++ wlghsp/DP/Boj9184_신나는함수실행.java
@@ -1,6 +1,5 @@
 package baekjoon;
 
-import java.io.IOException;
 
 /*
 
@@ -50,8 +49,54 @@ w(-1, 7, 18) = 1
 
 */
 
-public class Boj9184_신나는함수실행 {
-    public static void main(String[] args) throws IOException {
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
 
+public class Boj9184_신나는함수실행 {
+    // cindya님 코드 참조합니다.
+
+    // Map의 key와 value에 계산결과 저장하고 containsKey로 key를 확인하고 get으로 결과를 반환하는 방법
+    private static Map<String, Integer> memory = new HashMap<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        while (true) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            if (a == -1 && b == -1 && c == -1) break; // 입력이 -1 -1 -1 이면 멈춤
+
+            int res = w(a, b, c);
+            bw.write(String.format("w(%d, %d, %d) = %d\n", a, b, c, res)); // 출력
+        }
+        br.close();
+        bw.close();
+
+    }
+
+    private static int w(int a, int b, int c) {
+        int res;
+
+        String key = String.format("%d %d %d", a, b, c); // a, b, c를 문자열 키로 만듬
+        if (memory.containsKey(key)) // 이전에 같은 a b c 연산의 결과가 있으면
+            return memory.get(key); // 그 결과를 반환
+
+
+        if (a <= 0 || b <= 0 || c <= 0)
+            res = 1;
+        else if (a > 20 || b > 20  || c > 20)
+            res = w(20, 20, 20);
+        else if (a < b && b < c)
+            res = w(a, b, c - 1) + w(a, b -1, c -1 ) - w(a, b-1, c);
+        else
+            res = w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1);
+
+        memory.put(key, res); // 위 연산 결과를 저장
+
+        return res; // 결과 반환
     }
 }
