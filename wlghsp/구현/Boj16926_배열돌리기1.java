a82164a9b834292c4bd1296c686e4bc7544cf52d
wlghsp <wlghsp@gmail.com>
2021-12-02 08:43:21 +0900

    2021-12-02 동물원 | 배열돌리기1(미완)

---
+++ wlghsp/구현/Boj16926_배열돌리기1.java
@@ -0,0 +1,17 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Boj16926_배열돌리기1 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+		int h = Integer.parseInt(st.nextToken());
+		int w = Integer.parseInt(st.nextToken());
+		int n = Integer.parseInt(st.nextToken());
+    }
+}
