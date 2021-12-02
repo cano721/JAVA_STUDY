f7870b2c881a2c38217d9a336853606f6f040adb
jeeyani <74583344+jeeyani@users.noreply.github.com>
2021-12-02 22:45:05 +0900

    2021-12-02 2xn타일링_런타임 에러 수정

--- jeeyani/dynamicProgramming/BJ11726_2xn타일링.java
+++ jeeyani/dynamicProgramming/BJ11726_2xn타일링.java
@@ -1,8 +1,10 @@
 package dynamicProgramming;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 
 /*
@@ -25,26 +27,30 @@ import java.io.InputStreamReader;
 public class BJ11726_2xn타일링 {
 
 	static int n;
-	static long dp[];
+	static long[] dp;
 	
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
 		n = Integer.parseInt(br.readLine());
 		/*tile = new int[2][n]; */
 		
-		dp = new long[n+1]; //n+1 !!
+		dp = new long[n+2];
 		
+		int value = 10007;
 		
 		dp[1]=1;
 		dp[2]=2;
 		
 		for (int i = 3; i <=n; i++) {
 
-			dp[i] = (dp[i-1] +dp[i-2])%10007;
+			dp[i] = (dp[i-1] +dp[i-2])%value;
 		}
 		
-		System.out.println(dp[n]);
+		bw.write(dp[n] + "\n");
+        bw.flush();
+        bw.close();
 	}
 
 }
