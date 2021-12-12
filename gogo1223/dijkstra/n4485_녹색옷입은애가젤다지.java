8f2e3e612f3aacbc621b2c32675f080e7c43661a
1223gogo <73056953+1223gogo@users.noreply.github.com>
2021-12-13 08:00:31 +0900

    2021-12-13 녹색옷

---
+++ gogo1223/dijkstra/n4485_녹색옷입은애가젤다지.java
@@ -0,0 +1,22 @@
+package dijkstra;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class n4485_녹색옷입은애가젤다지 {
+	static int n, m;
+	static Bus[] e;
+	static long[] dist;
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+	}
+
+}
