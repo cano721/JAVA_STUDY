f366dfac6ff5c2760134398b7ce86c7cc400f7d0
cano721 <cano721@naver.com>
2021-12-26 09:09:38 +0900

    패스권 체크

---
+++ haebum/BJ2031_이쿠키달지않아.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+
+/**
+ * t분 n까지의 종류의 음식
+ * d분 지속되는 차
+ * k개의 차가 존재
+ * 
+ * 
+ * 
+ */
+public class BJ2031_이쿠키달지않아 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    }
+}
