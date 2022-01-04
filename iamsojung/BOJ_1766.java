f998c99ce872549a92cc4048765b3a6b0c4c3f33
iamsojung <sojungkid@gmail.com>
2022-01-05 00:17:09 +0900

    2022-01-04

--- iamsojung/BOJ_1766.java
+++ iamsojung/BOJ_1766.java
@@ -1,38 +1,52 @@
+package week1;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
-public class BOJ_14567 {
+public class BOJ_1766 {
 
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N, M;
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
-        int[] dp = new int[N + 1];
-        boolean[][] arr = new boolean[N + 1][N + 1];
+        int[] num = new int[N+1];
+        int[] map = new int[N+1];
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int A = Integer.parseInt(st.nextToken());
             int B = Integer.parseInt(st.nextToken());
-            arr[A][B] = true;
-        }
 
-        //dp[i] = i번째 과목이 이수할 수 있는 최소 학기 + 1
+            num[A] = B;
+        }
+        ArrayList<Integer> arr = new ArrayList<>();
+
+
         for (int i = 1; i <= N; i++) {
-            dp[i] = 1;
-            for (int j = 1; j < i; j++) {
-                if (arr[j][i]) {
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
-                }
+
+            if(num[i]==0){
+                arr.add(i);
+                map[i] = arr.size()-1;
+                System.out.println(i+" "+map[i]);
+            }else{
+
+                arr.add(map[num[i]],i);
+                map[i] = map[num[i]];
+                map[num[i]] = map[i]+1;
+
+                System.out.println(i+" "+map[num[i]]);
             }
         }
-        for (int i = 1; i <= N; i++) {
-            System.out.print(dp[i] + " ");
+
+        for (int i = 0; i < arr.size(); i++) {
+            System.out.println(arr.get(i));
         }
+
     }
 }
