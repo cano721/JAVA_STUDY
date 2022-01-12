44ba8b59d3cbbab2d94ca0d8274e98eaa39643c6
1223gogo <73056953+1223gogo@users.noreply.github.com>
2022-01-12 23:58:33 +0900

    2022-01-12 중복제거_수정

--- gogo1223/n13701_중복제거.java
+++ gogo1223/n13701_중복제거.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.BitSet;
 import java.util.StringTokenizer;
 
@@ -8,7 +10,7 @@ public class n13701_중복제거 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	    
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 	    int val;
 	    StringTokenizer st = new StringTokenizer(br.readLine());
 	    BitSet bs = new BitSet();
@@ -19,8 +21,9 @@ public class n13701_중복제거 {
 	        if(bs.get(val)) continue;
 	        bs.set(val);
 	        
-	        System.out.print(val+" ");
+	        bw.write(val+" ");
 	    }
+	    bw.flush();
 	}
 
 }
