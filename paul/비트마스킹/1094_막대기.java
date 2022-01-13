b0ed0d3611f53eb3f84daade2e86d2346fe30d87
puallab <pdh5056@gmail.com>
2022-01-14 03:49:53 +0900

    2022-01-14 막대기

---
+++ paul/비트마스킹/1094_막대기.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int cnt = Integer.bitCount(n);
+        System.out.println(cnt);
+    }
+}
