96adf69c332a08c804df4159e80423fa98934990
cano721 <cano721@naver.com>
2022-01-24 20:05:21 +0900

    2022-01-24 다트게임,비밀지도

---
+++ haebum/전체유형 프로그래머스/다트게임.java
@@ -0,0 +1,65 @@
+/**
+    문자열 파싱을 어떻게 할 것인가?
+    
+    문자열을 포문을 돌면서 쌓기
+    1. 문자가 나오면 앞에 숫자를 인트형으로 변환
+    
+    2. 문자에따른 곱하기 진행
+    
+    3. 배열에 담기
+    3-1) 배열 인덱스를 만들어두고 증가하는 형태
+    3-2) 담은후엔 임시 문자열 초기화
+    
+    4. * 이나 #이 나오면 해당하는 배열원소를 변경
+    4-1) 현재 배열 인덱스에따른 처리
+    
+    5. 최종 배열 합계 출력
+    
+    
+
+
+**/
+class Solution {
+    public int solution(String dartResult) {
+        int answer = 0;
+        
+        int[] arr = new int[3];
+        String str = "";
+
+        int idx = 0;
+        for(int i = 0; i < dartResult.length(); i++){
+            // 스타상
+            if(dartResult.charAt(i) == '*'){
+                if(idx == 1){
+                    arr[idx-1] *= 2;
+                }else{
+                    arr[idx-1] *= 2;
+                    arr[idx-2] *= 2;
+                }
+            // 아차상
+            }else if(dartResult.charAt(i) == '#'){
+                arr[idx-1] *= -1;
+            // 숫자일 경우
+            }else if(dartResult.charAt(i) < 'A'){
+                str += String.valueOf(dartResult.charAt(i) -'0');
+            // 문자
+            }else{
+                int score = Integer.parseInt(str);
+                if(dartResult.charAt(i) == 'S'){
+                    arr[idx++] = score;
+                }else if(dartResult.charAt(i) == 'D'){
+                    arr[idx++] = (int)Math.pow(score,2);
+                }else{
+                    arr[idx++] = (int)Math.pow(score,3);
+                }
+                // 초기화
+                str = "";
+            }
+        }
+        
+        for(int v: arr){
+            answer += v;
+        }
+        return answer;
+    }
+}
---
+++ haebum/전체유형 프로그래머스/비밀지도.java
@@ -0,0 +1,44 @@
+/**
+    1. 비트마스크
+    
+    2. 배열 풀이
+    
+    비트마스크 풀이
+    
+    1) 지도1과 지도2를 배열에 저장
+    
+    2) 두개의 합집합을 해서 새로운 배열에 담기
+    
+    3) 배열의 원소를 5자리 바이너리로 뽑아서 # 또는 공백 출력
+    3-1) 모자른 자리수 만큼 앞에 공백 집어넣기
+**/
+
+class Solution {
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = new String[n];
+        int[] map = new int[n];
+        
+        // 합집합
+        for(int i = 0; i < n; i++){
+            map[i] = arr1[i] | arr2[i];
+        }
+        
+        // 맵 돌면서 출력문 만들기
+        for(int i = 0; i < n; i++){
+            int num = map[i];
+            String str = "";
+            // 이진수 뽑기
+            String code = Integer.toBinaryString(num);
+            // 자리수 맞추기
+            for(int j = 0; j < (n-code.length()); j++){
+                str += " ";
+            }
+            // 이진수 해당 문자로 변경
+            for(int j = 0; j < code.length(); j++){
+                str += (code.charAt(j) == '1') ? "#" :" ";
+            }
+            answer[i] = str;
+        }
+        return answer;
+    }
+}
