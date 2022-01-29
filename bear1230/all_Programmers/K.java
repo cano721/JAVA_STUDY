ebe890d8b74f7cecae6a28ed9b39e6f35931b99d
gomdol <oioifor@naver.com>
2022-01-30 00:07:41 +0900

    2022-01-29 k진수에서 소수 개수 구하기

---
+++ bear1230/all_Programmers/K.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Solution {
+	public int solution(int n, int k) {
+		int answer = 0;
+
+		StringBuilder sb = new StringBuilder();
+		while (n >= k) {
+			sb.append(n % k);
+			n = n / k;
+		}
+		sb.append(n);
+		String res = sb.reverse().toString();
+
+		String[] arr = res.split("0");
+		for (String s : arr) {
+			if (!s.isEmpty() && checkNum(Long.valueOf(s))) {
+				answer += 1;
+			}
+		}
+		return answer;
+	}
+
+	static public boolean checkNum(long n) {
+		if (n <= 1)
+			return false;
+		else if (n == 2)
+			return true;
+		for (int i = 2; i <= Math.sqrt(n); i++) {
+			if (n % i == 0)
+				return false;
+		}
+
+		return true;
+	}
+}
