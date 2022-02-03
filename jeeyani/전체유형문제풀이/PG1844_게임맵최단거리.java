00c9668355feb89702866870cb6673dccf06f571
jeeyani <74583344+jeeyani@users.noreply.github.com>
2022-02-03 23:28:02 +0900

    2022-02-03 게임맵최단거리(테스트1,5,19_런타임에러)ㅜㅠ

--- jeeyani/전체유형문제풀이/PG1844_게임맵최단거리.java
+++ jeeyani/전체유형문제풀이/PG1844_게임맵최단거리.java
@@ -16,41 +16,49 @@ public class PG1844_게임맵최단거리 {
 
 	static int[] dx = { -1, 1, 0, 0 };
 	static int[] dy = { 0, 0, -1, 1 };
+	static boolean[][] visited;
 
 	public static int solution(int[][] maps) {
-		int answer = -1;
+int answer = -1;
+        
+        int n = maps.length;
+        int m = maps[0].length;
+        visited = new boolean[m][m];
+        
+        Queue<nodeUser> q = new LinkedList<>();
+        q.offer(new nodeUser(0,0)); //사용자의 캐릭터 시작위치
+        visited[0][0] = true;
+        
+        while(!q.isEmpty()){
+            nodeUser user = q.poll();
+            int x = user.x;
+            int y = user.y;
+            
+            //도착했으면 종료
+            if(x == n-1 && y == m-1) {
+                 answer = maps[x][y];
+                break;
+            }
+            
+            for(int i = 0; i<4; i++){
+                
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                
+                //범위와 벽인 경우 무시
+                if(nx < 0 || nx >= n || ny < 0 || ny >=m) continue;
+                if(maps[nx][ny] == 0) continue;
+                if(visited[nx][ny]) continue;
+                
 
-		Queue<nodeUser> q = new LinkedList<>();
-		q.add(new nodeUser(0, 0)); //사용자의 캐릭터 시작위치
+                maps[nx][ny] = maps[x][y] + 1;
+                q.offer(new nodeUser(nx, ny));
+                visited[nx][ny] = true;
 
-		while (!q.isEmpty()) {
-			nodeUser user = q.poll();
-			int x = user.x;
-			int y = user.y;
-
-			for (int i = 0; i < 4; i++) {
-
-				int nx = x + dx[i];
-				int ny = y + dy[i];
-
-				//범위와 벽인 경우 무시
-				if (nx < 0 || nx >= 5 || ny < 0 || ny >= 5)
-					continue;
-				if (maps[nx][ny] == 0)
-					continue;
-
-				if (maps[nx][ny] == 1) {
-					maps[nx][ny] = maps[x][y] + 1;
-					q.add(new nodeUser(nx, ny));
-				}
-			}
-		}
-
-		if (maps[4][4] > 1) {
-			answer = maps[4][4];
-		}
-
-		return answer;
+            }
+        }
+        
+        return answer;
 	}
 
 	public static class nodeUser {
