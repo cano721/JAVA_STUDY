900898a81496353ae7baffdb6a3614118458c779
cano721 <cano721@naver.com>
2022-02-17 00:02:05 +0900

    2022-02-17 카드 짝 맞추기(미완성)

--- haebum/전체유형 프로그래머스/카드 짝 맞추기.java
+++ haebum/전체유형 프로그래머스/카드 짝 맞추기.java
@@ -1,20 +1,12 @@
 import java.util.*;
 
-public class Test {
-
+class Solution {
     public static int[] card, choice;
     public static int w,h;
     public static int[] dirX = {0,0,1,-1};
     public static int[] dirY = {1,-1,0,0};
-    public static Map<Integer,ArrayList<int[]>> map = new HashMap<>();
     public static int answer = Integer.MAX_VALUE;
-    public static void main(String[] args) {
-        int r = 1;
-        int c = 0;
-        int[][] board = {{1,0,0,3},{2,0,0,0},{0,0,0,2},{3,0,1,0}};
-        solution(board, r, c);
-    }
-
+    
     public static int solution(int[][] board, int r, int c) {
         
         int cardNum = cardNum(board);
@@ -25,7 +17,6 @@ public class Test {
         h = board.length;
         
         cardSequence(board,1,r,c);
-        System.out.println(answer);
         return answer;
     }
     
@@ -42,34 +33,11 @@ public class Test {
         for(int i = 1; i < choice.length; i++){
             int num = choice[i];
             
-            int[] end1 = map.get(num).get(0);
-            int[] end2 = map.get(num).get(1);
-
-            int[] pos1 = bfs(r, c, board, end1, totalVisited);
-            int[] pos2 = bfs(r, c, board, end2, totalVisited);
-
-            if(pos1[2] < pos2[2]){
-                r = pos1[0];
-                c = pos1[1];
-                cnt += pos1[2];
-                arr.add(new int[] {r,c});
-                totalVisited[r][c] = true;
-                int[] pos3 = bfs(r, c, board, end2, totalVisited);
-                r = pos3[0];
-                c = pos3[1];
-                cnt += pos3[2];
-                arr.add(new int[] {r,c});
-                totalVisited[r][c] = true;
-            }else{
-                r = pos2[0];
-                c = pos2[1];
-                cnt += pos2[2];
-                arr.add(new int[] {r,c});
-                totalVisited[r][c] = true;
-                int [] pos3 = bfs(r, c, board, end1, totalVisited);
-                r = pos3[0];
-                c = pos3[1];
-                cnt += pos3[2];
+            for(int j = 0; j < 2; j++){
+                int[] pos = bfs(r,c,newBoard,num,totalVisited);
+                r = pos[0];
+                c = pos[1];
+                cnt += pos[2];
                 arr.add(new int[] {r,c});
                 totalVisited[r][c] = true;
             }
@@ -83,11 +51,11 @@ public class Test {
     }
     
     // 카드 맞추기 위한 조작 카운트 함수
-    public static int[] bfs(int r, int c, int[][] board, int[] end,boolean[][] totalVisited){
+    public static int[] bfs(int r, int c, int[][] board, int num,boolean[][] totalVisited){
         
         boolean[][] visited = new boolean[h][w];
         
-        if(r == end[0] && c == end[1] && totalVisited[r][c] == false){
+        if(board[r][c] == num && totalVisited[r][c] == false){
             return new int[] {r,c,1};
         }
         
@@ -104,7 +72,7 @@ public class Test {
             int cost = cur[2];
             
             // 원하는 숫자면 탈출
-            if(x == end[0] && y == end[1] && totalVisited[x][y] == false){
+            if(board[x][y] == num && totalVisited[x][y] == false){
                 return new int[] {x,y,cost+1};
             }
             
@@ -183,18 +151,16 @@ public class Test {
     
     // 카드 개수 파악 함수
     public static int cardNum(int[][] board){
-        for(int i = 0; i < board.length; i++){
-            for(int j = 0; j < board[0].length; j++){
-                if(board[i][j] != 0){
-                    if(!map.containsKey(board[i][j])){
-                        map.put(board[i][j],new ArrayList<>());
-                    }
-                    map.get(board[i][j]).add(new int[] {i,j});
+        Set<Integer> set = new HashSet<>();
+        
+        for(int[] b : board){
+            for(int card : b){
+                if(card != 0){
+                    set.add(card);
                 }
             }
         }
         
-        return map.size();
+        return set.size();
     }
-
 }
