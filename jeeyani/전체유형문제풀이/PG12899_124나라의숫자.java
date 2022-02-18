4f890ef2048b1f9b1c5cbe8fe952733c9eec2a59
jeeyani <74583344+jeeyani@users.noreply.github.com>
2022-02-18 14:14:49 +0900

    2022-02-18 124 나라의 숫자

---
+++ jeeyani/전체유형문제풀이/PG12899_124나라의숫자.java
@@ -0,0 +1,32 @@
+package 전체유형문제풀이;
+
+import java.util.*;
+
+
+public class PG12899_124나라의숫자 {
+
+	public static void main(String[] args) {
+
+		int n = 4;
+
+		String result = solution(n);
+
+		System.out.println(result);
+
+	}
+	
+	static String[] num = {"4","1","2"};
+
+	private static String solution(int n) {
+		String answer = "";
+	       
+        while(n > 0){
+            answer = num[n%3] + answer;
+            n =(n-1)/3;
+        }
+        
+        
+        return answer;
+	}
+
+}
