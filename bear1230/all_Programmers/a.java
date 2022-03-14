ef71b8d2dbe5fca28d30d66194808aefba8a27ce
gomdol <oioifor@naver.com>
2022-03-15 03:28:17 +0900

    2022-03-15 구명보트, 교점에 별 만들기

---
+++ bear1230/all_Programmers/a.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        Arrays.sort(people);
+        int min = 0;
+        for (int max = people.length - 1; min <= max; max--){
+            if (people[min] + people[max] <= limit) min++;
+      
+            answer++;
+        }
+            
+        return answer;
+    }
+}
---
+++ bear1230/all_Programmers/start.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+class Solution {    
+    public String[] solution(int[][] line) {
+        int minX = Integer.MAX_VALUE;
+        int minY = Integer.MAX_VALUE;
+        int maxX = Integer.MIN_VALUE;
+        int maxY = Integer.MIN_VALUE;
+
+        HashSet<String> hash = new HashSet<>();
+        for(int i=0;i<line.length-1;i++){
+            for(int j=i+1;j<line.length;j++){
+                int tmp = line[i][0]*line[j][1] - line[i][1]*line[j][0];
+                int tx = line[i][1]*line[j][2] - line[i][2]*line[j][1];
+                int ty = line[i][2]*line[j][0] - line[i][0]*line[j][2];
+                if(tmp==0 || tx%tmp!=0 || ty%tmp!=0 ) continue; 
+
+                int x = tx/tmp;
+                int y = ty/tmp;
+
+                hash.add(x+","+y);
+                minX = Math.min(minX,x);
+                minY = Math.min(minY,y);
+                maxX = Math.max(maxX,x);
+                maxY = Math.max(maxY,y);
+            }
+        }
+        if(minX==Integer.MAX_VALUE){
+            String [] answer = new String [1];
+            answer[0] = "*";
+            return answer;
+        }
+        String [] answer = new String [maxY - minY + 1];
+        int idx = 0;
+        for(int i=maxY;i>=minY;i--){
+            StringBuilder sb = new StringBuilder();
+            for(int j=minX;j<=maxX;j++){
+                if(hash.contains(j+","+i))
+                    sb.append("*");
+                else
+                    sb.append(".");
+            }
+            answer[idx++] = sb.toString();
+        }
+        return answer;
+    }
+}
