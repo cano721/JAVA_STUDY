9e3509f2b0cef22f87eb907d79b23411013f91f3
1223gogo <73056953+1223gogo@users.noreply.github.com>
2022-03-15 08:34:31 +0900

    교점에 별만들기 마지막 테스트케이스 실패

--- gogo1223/programmers/d220315_교점에별만들기.java
+++ gogo1223/programmers/d220315_교점에별만들기.java
@@ -1,5 +1,10 @@
 package programmers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map.Entry;
+
 public class d220315_교점에별만들기 {
 
 	public static void main(String[] args) {
@@ -9,10 +14,53 @@ public class d220315_교점에별만들기 {
 			System.out.println(ans[i]);
 		}
 	}
-
+//	Ax + By + E = 0
+//	Cx + Dy + F = 0
+//	x = (BF - ED)/(AD - BC), y = (EC - AF)/(AD - BC)
 	private static String[] solution(int[][] line) {
-		// TODO Auto-generated method stub
-		return null;
+		HashMap<Integer, ArrayList<Integer>> map = new HashMap<>();
+		int minX = Integer.MAX_VALUE, minY = Integer.MAX_VALUE,
+				maxX = Integer.MIN_VALUE, maxY = Integer.MIN_VALUE;
+		for (int i = 0; i < line.length - 1; i++) {
+			for (int j = i+1; j < line.length; j++) {
+				//평행 여부 판별
+				int AD_BC = line[i][0]*line[j][1] - line[i][1]*line[j][0];
+				if(AD_BC == 0) continue;
+				//정수 여부 판별
+				int BF_ED = line[i][1]*line[j][2] - line[i][2]*line[j][1];
+				int EC_AF = line[i][2]*line[j][0] - line[i][0]*line[j][2];
+				if(BF_ED % AD_BC != 0 || EC_AF % AD_BC != 0) continue;
+				
+				int x = BF_ED / AD_BC;
+				int y = EC_AF / AD_BC;
+				//x, y 순서쌍 저장
+				if(map.containsKey(y)) {
+					if(map.get(y).contains(x)) continue;
+					else map.get(y).add(x);
+				}else {
+					map.put(y, new ArrayList<>());
+					map.get(y).add(x);
+				}
+				minX = Math.min(minX, x);
+				minY = Math.min(minY, y);
+				maxX = Math.max(maxX, x);
+				maxY = Math.max(maxY, y);
+			}
+		}
+		String[] answer = new String[maxY - minY + 1];
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < maxX - minX + 1; i++) {
+			sb.append(".");
+		}
+		Arrays.fill(answer, sb.toString());
+		for(Entry<Integer, ArrayList<Integer>> entry : map.entrySet()) {
+			int ny = maxY - entry.getKey();
+			for (int i = 0; i < entry.getValue().size(); i++) {
+				int nx = entry.getValue().get(i) - minX;
+				answer[ny] = answer[ny].substring(0, nx) + "*" + answer[ny].substring(nx+1);
+			}
+		}
+		return answer;
 	}
 
 }
