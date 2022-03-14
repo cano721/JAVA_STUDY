b33591676832690dfcecd132913a94e4a188c143
jiyani <jiyani@112.187.137.171>
2022-03-14 20:44:44 +0900

    커밋테스트

---
+++ jeeyani/전체유형문제풀이/PG42885_구명보트.java
@@ -0,0 +1,25 @@
+package 전체유형문제풀이;
+
+import java.util.*;
+
+
+public class PG42885_구명보트 {
+
+	public static void main(String[] args) {
+
+
+		int[] people = {70,50,80,50};
+		
+		int result = solution(people, 100);
+
+		System.out.println(result);
+
+	}
+
+	private static int solution(int[] people, int i) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+
+}
