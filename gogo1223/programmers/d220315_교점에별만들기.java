4fe0730135c715c855c1ba66f40325d89a131bd5
1223gogo <73056953+1223gogo@users.noreply.github.com>
2022-03-15 08:00:56 +0900

    2022-03-15 구명보트,교점에 별만들기

---
+++ gogo1223/programmers/d220315_교점에별만들기.java
@@ -0,0 +1,18 @@
+package programmers;
+
+public class d220315_교점에별만들기 {
+
+	public static void main(String[] args) {
+		int[][] line = {{2, -1, 4}, {-2, -1, 4}, {0, -1, 1}, {5, -8, -12}, {5, 8, 12}};
+		 String[] ans = solution(line);
+		 for (int i = 0; i < ans.length; i++) {
+			System.out.println(ans[i]);
+		}
+	}
+
+	private static String[] solution(int[][] line) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
