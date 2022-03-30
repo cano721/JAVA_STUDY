3902b0e5009a6f5cacd682beea1ba6d5f7022cd9
hobin <elwlahd555@naver.com>
2022-03-30 20:59:53 +0900

    2022-03-30 사라지는 발판 (실패)

--- elwlahd555/baekjoon/baekjoon21609_상어_중학교.java
+++ elwlahd555/baekjoon/baekjoon21609_상어_중학교.java
@@ -1,5 +1,160 @@
 package elwlahd555.baekjoon;
 
-public class baekjoon21609_상어_중학교 {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
+public class baekjoon21609_상어_중학교{	
+	static int N,M, answer;
+	static int[][] map;
+	static boolean[][] visited;
+	static LinkedList<Block> list = new LinkedList<>();
+	static Queue<int []> queue = new LinkedList<int[]>();
+	static int[] dx = {1,0,-1,0};
+	static int[] dy = {0,1,0,-1};
+	
+	static private class Block implements Comparable<Block>{
+		int totalPoint, rainbowPoint, row, col;
+		public Block(int totalPoint, int rainbowPoint, int row, int col) {
+			this.totalPoint = totalPoint;
+			this.rainbowPoint = rainbowPoint;
+			this.row = row;
+			this.col = col;
+		}
+		
+		public int compareTo(Block o) {
+			if(this.totalPoint == o.totalPoint) {
+				if(this.rainbowPoint == o.rainbowPoint) {
+					if(this.row == o.row) {
+						return o.col - this.col;
+					}
+					return o.row - this.row;
+				}
+				return o.rainbowPoint - this.rainbowPoint;
+			}
+			
+			return o.totalPoint - this.totalPoint;
+		}
+	}
+	
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		map = new int[N][N];
+		visited = new boolean[N][N];
+		for(int i = 0 ; i < N ; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			for(int j = 0 ; j < N ; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		while(true) {
+			// 블록 그룹 찾기 2 이상 아니면 break;
+			visited = new boolean[N][N];
+			for(int i = 0 ; i < N ; i++) {
+				for(int j = 0 ; j< N ; j++) {
+					if(map[i][j] > 0 && visited[i][j] == false) {
+						bfs(i, j, true);
+					}
+				}
+			}
+			if(list.isEmpty()) break;
+			
+			Collections.sort(list);
+			// 찾은 블록 없애기
+			visited = new boolean[N][N];
+			bfs(list.get(0).row, list.get(0).col, false);
+			removeBlock();
+
+			// 중력
+			gravity();
+			// 반시계
+			map = rotate();
+			// 중력 
+			gravity();
+			
+			list.clear();
+		}
+		System.out.println(answer);
+	}
+
+	private static int[][] rotate() {
+		int[][] temp = new int[N][N];
+		
+		for(int i = 0 ; i < N ; i++) {
+			for(int j = 0 ; j < N ; j++) {
+				temp[N-j-1][i]=map[i][j];
+			}
+		}
+		
+		return temp;
+	}
+
+	private static void gravity() {
+		for(int j = 0 ; j < N ; j++) {
+			for(int i = N-1 ; i >= 0 ; i--) {
+				for(int k = i ; k < N-1 ; k ++) {
+					if(map[k][j] == -1) continue;
+					if(map[k][j] != -2 && map[k+1][j] == -2) {
+						int temp = map[k][j];
+						map[k][j] = -2;
+						map[k+1][j] = temp;
+					}
+				}
+			}
+		}
+	}
+
+	private static void removeBlock() {
+		int count = 0;
+		for(int i = 0 ; i < N ; i++) {
+			for(int j = 0 ; j < N ; j++) {
+				if(visited[i][j] == true) {
+					count++;
+					map[i][j] = -2;
+				}
+			}
+		}
+		
+		answer += (int)Math.pow(count, 2);
+	}
+
+	private static void bfs(int x, int y, boolean flag) {
+		int blockPoint = map[x][y];
+		visited[x][y] = true;
+		queue.offer(new int[]{x, y});
+		int totalPoint = 1;
+		int rainbowPoint = 0;
+		while(! queue.isEmpty()) {
+			int cur[] = queue.poll();
+			for(int i  = 0 ; i < 4 ; i++) {
+				int nx = cur[0] + dx[i];
+				int ny = cur[1] + dy[i];
+				
+				if(0 > nx || nx >= N || 0 > ny || ny >= N || visited[nx][ny] == true) continue;
+				if(map[nx][ny] == blockPoint || map[nx][ny] == 0) {					
+					if(map[nx][ny] == 0) rainbowPoint += 1;
+					totalPoint += 1;
+					visited[nx][ny] = true;
+					queue.offer(new int[] {nx, ny});
+				}
+			}
+		}
+		
+		if(totalPoint >= 2) list.add(new Block(totalPoint, rainbowPoint, x, y));
+		if(flag == true) {
+			for(int i = 0; i < N ; i++) {
+				for(int j = 0 ; j < N ; j++) {
+					if(map[i][j] == 0) visited[i][j] = false;
+				}
+			}
+		}
+		
+	}
 }
---
+++ elwlahd555/programmers/프로그래머스92345_사라지는_발판.java
@@ -0,0 +1,71 @@
+package elwlahd555.programmers;
+
+public class 프로그래머스92345_사라지는_발판 {
+	public static void main(String[] args) {
+		int board[][]= {{1,1,1},{1,1,1},{1,1,1}};
+		int aloc[]= {1,0};
+		int bloc[]= {1,2};
+		System.out.println(solution(board, aloc, bloc));
+	}
+    private static int boardRowLen, boardColLen;
+    private static int dx[]= {-1,1,0,0};
+    private static int dy[]= {0,0,-1,1};
+    private static class Result{
+        boolean win;
+        int count;
+        
+        public Result(boolean win, int count){
+            this.win = win;
+            this.count = count;
+        }
+    }
+    public static int solution(int[][] board, int[] aloc, int[] bloc) {
+        boardRowLen=board.length;
+        boardColLen=board[0].length;
+        return dfs(aloc[0],aloc[1],bloc[0],bloc[1], 0, 0, board).count;
+    }
+	private static Result dfs(int ax, int ay, int bx, int by, int aDepth, int bDepth, int[][] board) {
+        boolean win = false;
+        int minCount = 5*5;
+        int maxCount = aDepth + bDepth;
+        
+        if(aDepth == bDepth && board[ax][ay] == 1){
+            for (int i = 0; i < 4; i++) {
+                int axTmp = ax + dx[i];
+                int ayTmp = ay + dy[i];
+                if(isValid(axTmp, ayTmp, board)){
+                    board[ax][ay] = 0;
+
+                    Result d = dfs(axTmp, ayTmp, bx, by, aDepth+1, bDepth, board);
+                    win = win || !d.win;
+                    if(!d.win) minCount = Math.min(minCount, d.count);
+                    else maxCount = Math.max(maxCount, d.count);
+
+                    board[ax][ay] = 1;
+                }
+            }
+        } else if (aDepth > bDepth && board[bx][by] == 1){
+            for (int i = 0; i < 4; i++) {
+                int bxTmp = bx + dx[i];
+                int byTmp = by + dy[i];
+                if(isValid(bxTmp, byTmp, board)){
+                    board[bx][by] = 0;
+
+                    Result d = dfs(ax, ay, bxTmp, byTmp, aDepth, bDepth+1, board);
+                    win = win || !d.win;
+                    if(!d.win) minCount = Math.min(minCount, d.count);
+                    else maxCount = Math.max(maxCount, d.count);
+
+                    board[bx][by] = 1;
+                }
+            }
+        }
+        
+        return new Result(win, win ? minCount : maxCount);
+    }
+    
+    public static boolean isValid(int x, int y, int[][] board){
+        if(x < 0 || x >= boardRowLen || y < 0 || y >= boardColLen || board[x][y] == 0) return false;
+        return true;
+    }
+}
