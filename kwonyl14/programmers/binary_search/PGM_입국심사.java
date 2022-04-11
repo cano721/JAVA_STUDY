30bdad20ade279e82d778557078d46656e971bdb
sqk8657 <sqk8657@naver.com>
2022-04-11 23:15:18 +0900

    2022-04-11 입국심사

---
+++ kwonyl14/programmers/binary_search/PGM_입국심사.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public long solution(int n, int[] times) {
+        long answer = 0;
+        Arrays.sort(times);
+        long left = 0;
+        long right = (long)n * times[times.length-1];
+        
+        while (left <= right) {
+            long mid = (left + right) / 2;
+            long sum = 0;
+            
+            for (int i = 0; i < times.length; i++) {
+                sum += mid / times[i];
+            }
+            
+            if (sum < n) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+                answer = mid;
+            }
+        }
+        
+        return answer;
+    }
+}
