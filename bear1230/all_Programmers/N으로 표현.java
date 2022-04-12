c678ec68a6676a7fd2da66bc97de6a8a90c250ed
gomdol <69491404+bear1230@users.noreply.github.com>
2022-04-13 02:58:24 +0900

    2022-04-13 N으로 표현

--- bear1230/all_Programmers/N으로 표현.java
+++ bear1230/all_Programmers/N으로 표현.java
@@ -10,7 +10,6 @@ class Solution {
 	public void DFS(int N, int number, int sum, int count) {
 		if (count > 8)
 			return;
-
 		if (sum == number) {
 			if (count < answer || answer == -1)
 				answer = count;
