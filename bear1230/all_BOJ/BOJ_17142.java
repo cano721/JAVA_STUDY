8b937d1309497ed67e58e490bd4b6b5f3a02613e
gomdol <69491404+bear1230@users.noreply.github.com>
2022-04-17 03:41:44 +0900

    2022-04-07 연구소3

--- bear1230/all_BOJ/BOJ_17142.java
+++ bear1230/all_BOJ/BOJ_17142.java
@@ -14,7 +14,6 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 
