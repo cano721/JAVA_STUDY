5de5ac8ccdfef39391767253a06a249713c15403
gomdol <oioifor@naver.com>
2022-05-28 01:55:09 +0900

    2022-05-28 풍선 터트리리기

---
+++ bear1230/all_Programmers/gg.java
@@ -0,0 +1,29 @@
+class Solution {
+	public int solution(int[] a) {
+		int answer = 2;
+		int[] fmin = new int[a.length];
+		int[] bmin = new int[a.length];
+		int tmp = Integer.MAX_VALUE;
+		for (int i = 0; i < a.length; i++) {
+			fmin[i] = Math.min(tmp, a[i]);
+			tmp = fmin[i];
+
+		}
+		tmp = Integer.MAX_VALUE;
+		for (int j = a.length - 1; j >= 0; j--) {
+			bmin[j] = Math.min(tmp, a[j]);
+			tmp = bmin[j];
+
+		}
+
+		for (int i = 1; i < a.length - 1; i++) {
+			int temp = a[i];
+			if (temp > fmin[i - 1] && temp > bmin[i + 1]) {
+				continue;
+			}
+			answer++;
+		}
+
+		return answer;
+	}
+}
