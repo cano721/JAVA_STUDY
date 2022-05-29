9705e1605bc89d2f3fe00e73bda7ebc191cb3d4e
sqk8657 <sqk8657@naver.com>
2022-05-29 20:55:33 +0900

    2022-05-29 욕심쟁이 판다 (메모리 초과 해결 실패)

---
+++ kwonyl14/baekjoon/BOJ_1937_욕심쟁이판다.java
@@ -0,0 +1,60 @@
+package day2205.day29;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_1937_욕심쟁이판다 {
+
+    static int N;
+    static int[][] map;
+    static int answer;
+    static int[] dr = {-1, 1, 0, 0};
+    static int[] dc = {0, 0, -1, 1};
+    static int[][] memo;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        N = Integer.parseInt(br.readLine());
+        map = new int[N][N];
+        memo = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (memo[i][j] > 0) continue;
+                dfs(i, j);
+            }
+        }
+        System.out.println(answer);
+    }
+
+    private static int dfs(int r, int c) {
+        int now = 1;
+        int max = 0;
+        for (int i = 0; i < 4; i++) {
+            int nr = r + dr[i];
+            int nc = c + dc[i];
+
+            if (nr < 0 || nc < 0 || nr >= N || nc >= N) continue;
+            if (map[nr][nc] < map[r][c]) continue;
+            if (memo[nr][nc] > 0) {
+                max = Math.max(max, memo[nr][nc]);
+            } else {
+                max = Math.max(max, dfs(nr, nc));
+            }
+        }
+
+        now += max;
+        memo[r][c] = now;
+        answer = Math.max(answer, now);
+        return now;
+    }
+}
